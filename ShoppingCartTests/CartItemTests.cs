1300efcfefb21566099809b9ee13ad4604f124c6
rraut <rraut@tavisca.com>
2017-07-31 14:24:19 +0530

    TDD with C# - Shpoppingcart

---
+++ ShoppingCartTests/CartItemTests.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using ShoppingCart;
+
+namespace ShoppingCartTests
+{
+    [TestFixture]
+    class CartItemTests
+    {
+        private int _quantity;
+        private string _description;
+        private CartItem _item;
+        private decimal _unitPrice;
+        private decimal _itemPrice;
+        private int _newQuantity;
+        private decimal _discountAmount;
+        private decimal _largeDiscount;
+        private decimal _zero;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _description = "Test Descritpion";
+            _quantity = 2;
+            _unitPrice = 1.00m;
+            _itemPrice = 2.00m;
+            _newQuantity = 3;
+            _discountAmount = 1.00m;
+            _largeDiscount = 3.00m;
+            _zero = 0;
+            _item = new CartItem(_quantity, _description, _unitPrice);
+        }
+
+        [Test]
+        public void ItemsHasQuantity()
+        {
+            Assert.That(_item.Quantity, Is.EqualTo(_quantity));
+        }
+
+        [Test]
+        public void ItemHasDescription()
+        {
+            Assert.That(_item.Description, Is.EqualTo(_description));
+        }
+
+        [Test]
+        public void ItemHasUnitPrice()
+        {
+            Assert.That(_item.UnitPrice, Is.EqualTo(_unitPrice));
+        }
+
+        [Test]
+        public void ItemPriceHasQuantityTimesUnitPrice()
+        {
+            var actual = _item.GetItemPrice();
+            Assert.That(actual, Is.EqualTo(_itemPrice));
+        }
+
+        [Test]
+        public void QuantityIsModifiable()
+        {
+            _item.Quantity = _newQuantity;
+            Assert.That(_item.Quantity, Is.EqualTo(_newQuantity));
+        }
+
+        [Test]
+        public void DiscountCanBeAppliedToItem()
+        {
+            _item.ApplyDiscount(_discountAmount);
+            Assert.That(_item.Discount, Is.EqualTo(_discountAmount));
+        }
+
+        [Test]
+        public void ItemPriceWithDiscountCannotBelEssThanZero()
+        {
+            _item.ApplyDiscount(_largeDiscount);
+            Assert.That(_item.GetItemPrice(), Is.EqualTo(_zero));
+        }
+    }    
+}
---
+++ ShoppingCartTests/ShoppingCartTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ShoppingCart;
+
+namespace ShoppingCartTests
+{
+    [TestFixture]
+    class ShoppingCartTest
+    {
+        private ShoppingCart _cart;
+        private CartItem _item;
+        private CartItem _item2;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _cart = new ShoppingCart();
+            _item = new CartItem(2, "Test", 1.00m);
+            _item2 = new CartItem(3, "Test", 2.00m);
+        }
+         
+        [Test]
+        public void ShoppingCartCanContainZeroItem()
+        {
+            Assert.That(_cart.Items.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ShoppingCartCanAddItem()
+        {
+            _cart.AddItem(_item);
+            Assert.That(_cart.Items, Has.Member(_item));
+        }
+
+        [Test]
+        public void CartContainsNoDuplicateItem()
+        {
+            _cart.AddItem(_item);
+            _cart.AddItem(_item);
+            Assert.That(_cart.Items.Count,Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CartRemoveItem()
+        {
+            _cart.AddItem(_item);
+            _cart.RemoveItem(_item);
+
+            Assert.That(_cart.Items.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetTotalShoppingCartPrice()
+        {
+            _cart.AddItem(_item);
+            _cart.AddItem(_item2);
+
+            Assert.That(_cart.GetShoppingCartTotal(),Is.EqualTo(8.00m));
+        }
+    }
+
+    public class ShoppingCart
+    {
+        public List<CartItem> Items { get; private set; }
+
+        public ShoppingCart()
+        {
+            Items = new List<CartItem>();
+        }
+
+        public void AddItem(CartItem item)
+        {
+            if (Items.Contains(item))
+                return;
+            Items.Add(item);
+        }
+
+        public void RemoveItem(CartItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public decimal GetShoppingCartTotal()
+        {
+            decimal total = 0;
+            foreach (var item in Items)
+            {
+                total += item.GetItemPrice();
+            }
+            return total;
+        }
+    }
+}
